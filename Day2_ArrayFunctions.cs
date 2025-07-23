85bd087844e5fb1c82cae019b4a4396d68c5308a
Zeba Haider <zeba.haider@hatch.com>
2025-07-23 12:35:17 +0530

    Added ref, out, and array as parameter examples

--- Day2_ArrayFunctions.cs
+++ Day2_ArrayFunctions.cs
@@ -40,11 +40,48 @@ class HelloWorld
         Console.WriteLine();
     }
 
+    //reference type function
+    static int fun(ref int num)
+    {
+        int sum = num + 1;
+        return sum;
+    }
 
+    //out parameter
+    static int fun2(int num, out int result)
+    {
+        result = num * num;
+        return result;
+    }
+
+    //array as parameter
+    static int arr(int[] arry)
+    {
+        for (int i = 0; i < arry.Length; i++)
+        {
+            arry[i] = 0;
+        }
+
+        return 0;
+    }
 
     static void Main()
     {
+        int x1 = 5;
+        int a1 = fun(ref x1);
+        Console.WriteLine(a1);
 
+        int prod;
+        Console.WriteLine(fun2(2, out prod));
+
+
+        int[] b1 = { 1, 3, 6, 8, 9 };
+        arr(b1);
+
+        for (int i = 0; i < b1.Length; i++)
+        {
+            Console.WriteLine(b1[i]);
+        }
         int[] myArray = { 2, 5, 3, 7, 4, 9 };
 
         PrintArray(myArray);
